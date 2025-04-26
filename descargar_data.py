2cdd8a1d3c303a07a835f0cd69c7ddedef64ee65
Samuel Colmanerez <samuelecf@hotmail.com>
2025-04-26 17:13:33 -0600

    fix descargar_py

--- descargar_data.py
+++ descargar_data.py
@@ -11,7 +11,9 @@ def create_data_folder():
 
 
 def download_data_from_s3(bucket_name, remote_path, local_path):
-    s3 = boto3.client('s3')
+    s3 = boto3.client('s3',
+    aws_access_key_id = secrets.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key= secrets.AWS_SECRET_ACCESS_KEY,)
     try:
         s3.download_file(bucket_name, remote_path, local_path)
         print(f"Datos descargados desde {bucket_name}/{remote_path} a {local_path}")
@@ -21,8 +23,8 @@ def download_data_from_s3(bucket_name, remote_path, local_path):
 
 if __name__ == "__main__":
     bucket_name = 'samuelstorageformlops2025'
-    remote_path = 'Hipertension_Arterial_Mexico.csv'
-    local_path = '/data/Hipertension_Arterial_Mexico.csv'
+    remote_path = 'data/Hipertension_Arterial_Mexico.csv'
+    local_path = '.\data\Hipertension_Arterial_Mexico.csv'
 
     create_data_folder()
     download_data_from_s3(bucket_name, remote_path, local_path)
